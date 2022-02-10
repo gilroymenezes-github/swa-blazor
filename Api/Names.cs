45e8b2451f98d405f41f039d2228e3c5d8d02cdc
gilroymenezes-github <gilroymenezes@gmail.com>
2022-02-10 18:43:20 +0530

    Added conneciton string format for static web app

--- Api/Names.cs
+++ Api/Names.cs
@@ -30,7 +30,7 @@ namespace BlazorApp.Api
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            [Table("names", Connection = "AzureWebJobsStorage")] CloudTable namesTable)
+            [Table("names", Connection = "AZURE_STORAGE_CONNECTION_STRING")] CloudTable namesTable)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
