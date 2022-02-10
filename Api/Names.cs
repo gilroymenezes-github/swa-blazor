c9fb524241b8d0df770bfc6c8ef82349d9698432
gilroymenezes-github <gilroymenezes@gmail.com>
2022-02-10 17:02:37 +0530

    added new razor for names endpoint

--- Api/Names.cs
+++ Api/Names.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
+using BlazorApp.Shared;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Cosmos.Table;
@@ -14,16 +15,16 @@ using Newtonsoft.Json;
 
 namespace BlazorApp.Api
 {
-    public class Users
+    public class Names
     {
-        private readonly ILogger<Users> _logger;
+        private readonly ILogger<Names> _logger;
 
-        public Users(ILogger<Users> log)
+        public Names(ILogger<Names> log)
         {
             _logger = log;
         }
 
-        [FunctionName(nameof(Users))]
+        [FunctionName(nameof(Names))]
         [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
@@ -38,7 +39,7 @@ namespace BlazorApp.Api
                 string name = req.Query["name"];
 
                 var filter = TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "HttpTrigger");
-                var query = new TableQuery<FieldEntity>().Where(filter);
+                var query = new TableQuery<NameEntity>().Where(filter);
                 var result = await namesTable.ExecuteQuerySegmentedAsync(query, new TableContinuationToken());
 
                 return new OkObjectResult(result);
@@ -48,12 +49,6 @@ namespace BlazorApp.Api
                 return new NotFoundObjectResult(ex);
             }
         }
-    }
-
-    internal class FieldEntity: TableEntity
-    {
-        public string? Field { get; set; }
-        public string? Id { get; set; }
     }
 }
 
---
+++ Shared/NameEntity.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorApp.Shared
+{
+    public class NameEntity : TableEntity
+    {
+        public string Field { get; set; }
+        public string Id { get; set; }
+    }
+}
